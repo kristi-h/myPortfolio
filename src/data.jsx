4eed7ae23e431241b56c264e92671ed1d38b2ee7
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-05-22 19:20:01 -0700

    initialize project with about, navbar, project, skills components

---
+++ src/data.jsx
@@ -0,0 +1,52 @@
+export const projects = [
+    {
+      title: "myLibrary",
+      subtitle: "React, React Router, React Hooks, Firebase",
+      description: "Log all the books you read with personal commentaries.",
+      link: "",
+      image: "",
+    },
+    {
+      title: "Vocabulary Builder",
+      subtitle: "React, React Router",
+      description:
+        "A flashcard app to test your vocabulary",
+      image: "",
+      link: "github",
+    },
+    {
+      title: "cinerama",
+      subtitle: "React and Firebase",
+      description: "I created a movie list app where you can search for movies and add them to your customized list. Each movie will populate a title, description, cover which you can add to a list of favorites by genre or 'want to watch'. Then share them with friends on social media.",
+      image: "",
+      link: "",
+    },
+    {
+      title: "myDoggo",
+      subtitle: "React",
+      description:
+        "Learn different how to identify different breeds of dogs then test your new skillset by testing your knowledge!",
+      image: "",
+      link: "github",
+    },
+    {
+      title: "quizzler",
+      subtitle: "React",
+      description:
+        "A trivia app to test your knowledge in different genres",
+      image: "",
+      link: "github",
+    },
+  ];
+  
+  export const skills = [
+    "JavaScript",
+    "React",
+    "React Router",
+    "React Hooks",
+    "Python",
+    "Tailwind",
+    "Node",
+    "CSS3",
+  ];
+  
