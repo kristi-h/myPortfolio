9d4ce5ab850cb747399ed21fde2f513f49f5411b
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-07-16 15:56:28 -0700

    Add about page on navbar and fix link for nav header to redirect to home, not about

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -6,11 +6,12 @@ export default function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <Link className='title-font font-medium text-white mb-4 md:mb-0 ml-3 text-xl' to='/about'>KH</Link>
+      <Link className='title-font font-medium text-white mb-4 md:mb-0 ml-3 text-xl' to='/'>KH</Link>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <Link className='mr-5 hover:text-white' to='/projects'>Projects</Link>
           <Link className='mr-5 hover:text-white' to='/skills'>Skills</Link>
           <Link className='mr-5 hover:text-white' to='/contact'>Contact</Link>
+          <Link className='mr-5 hover:text-white' to='/about'>About</Link>
         </nav>
       </div>
     </header>
